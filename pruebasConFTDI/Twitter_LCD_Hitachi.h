3f2e35a015dbc2ba69b8cd30c942ddd7a05626f5
ALE <aleronpower@hotmail.com>
2019-10-19 16:18:04 -0300

    Antes de continuar, voy a probar la libreria Chrono

--- pruebasConFTDI/Twitter_LCD_Hitachi.h
+++ pruebasConFTDI/Twitter_LCD_Hitachi.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Twitter_LCD.h"
+
+
 class Twitter_LCD_Hitachi :
 	public Twitter_LCD
 {
@@ -9,7 +11,14 @@ public:
 
 	virtual void step(void);
 private:
+	//MODO SHOW_TWEET
+	std::string formattedCurrent;
+	std::string formattedDate;
 	std::string formattedTweet;
+
+	//MODO LOADING
+	std::string formattedUser;
+	bool scrollUser;
 
 
 };
