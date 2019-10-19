02d416395778771573ce37163b64db3d5901d045
ALE <aleronpower@hotmail.com>
2019-10-19 20:39:53 -0300

    esto me esta llevando mas de lo planeado

--- pruebasConFTDI/Twitter_LCD_Hitachi.cpp
+++ pruebasConFTDI/Twitter_LCD_Hitachi.cpp
@@ -1,5 +1,14 @@
 #include "Twitter_LCD_Hitachi.h"
 
+#define BLANK_LINE "                " //Es mas rapido para el programa escribir espacios al final del mensaje, en vez de tener que limpiar por completo la linea y escribir lo correspondiente
+										//Notese q lo q hace el programa para borrar una sola linea es, de hecho, escribir espacios
+
+static const char LoadingArray[] = "|{(<({|})>)}";
+#define LOADING_INTERVAL 250 //mseg
+
+using namespace std;
+using namespace std::chrono;
+
 Twitter_LCD_Hitachi::Twitter_LCD_Hitachi()
 	: Twitter_LCD(),
 
@@ -13,8 +22,6 @@ Twitter_LCD_Hitachi::Twitter_LCD_Hitachi()
 	currentLoadSymbol(0),
 	loadSymbolTime(),
 
-
-	formattedTweet()
 {
 }
 
@@ -24,33 +31,116 @@ Twitter_LCD_Hitachi::~Twitter_LCD_Hitachi()
 
 void Twitter_LCD_Hitachi::step(void)
 {
-	if (newChanges == true) {
-		newChanges = false;
-		string message = "";
-		switch (status) {
-		case statusType::WELCOME:
-			cursorPosition start;
-			start.row = 0;
-			start.column = 0;
-			(*lcd).lcdSetCursorPosition(start);
-			(*lcd) << (const unsigned char*)"WELCOME TO TP9   TWITTER HITACHI";
-			break;
-		case statusType::GOODBYE:
-			(*lcd).lcdClear();
-			break;
-		case statusType::FINISHED_LOADING:
-			message += "FINISHED LOADINGTweets: ";
-			message += std::to_string(numberOfTweets);
-			(*lcd) << (unsigned const char*)message.c_str();
-			break;
-		case statusType::STOPPED_LOADING:
-			message += "STOPPED LOADING Tweets: ";
-			message += std::to_string(numberOfTweets);
-			(*lcd) << (unsigned const char*)message.c_str();
-			break;
+	if (lcdReadyToUse) {
+		if (newChanges == true) {
+			control_NewChanges();
+		}
+		else {
+			control_NoChanges();
 		}
 	}
-	else {
+}
 
+void Twitter_LCD_Hitachi::control_NewChanges(void)
+{
+	newChanges = false;
+	(*lcd).lcdClear();
+	string message;
+	switch (status) {
+	case statusType::WELCOME:
+		(*lcd) << (const unsigned char*)"WELCOME TO TP9   TWITTER HITACHI";
+		break;
+	case statusType::GOODBYE:
+		//El LCD ya esta en blanco
+		break;
+	case statusType::FINISHED_LOADING:
+		message = "FINISHED LOADINGTweets: ";
+		message += std::to_string(numberOfTweets);
+		(*lcd) << (unsigned const char*)message.c_str();
+		break;
+	case statusType::STOPPED_LOADING:
+		message = "STOPPED LOADING Tweets: ";
+		message += std::to_string(numberOfTweets);
+		(*lcd) << (unsigned const char*)message.c_str();
+		break;
+	case statusType::LOADING:
+		phraseToScroll = '@';
+		phraseToScroll += user;
+		if ((sizeofPhrase = phraseToScroll.size()) > 16) {
+			scrollPhase = true;
+			phraseToScroll += BLANK_LINE;
+			beginningOfPhrase = true;
+			timeBetweenScrolls = interval((time_interval_type)(1000. / speed));
+			lastScroll = system_clock::now();
+			actualPosition = 0;
+			//Imprime por primera vez el nombre
+			char c[17];
+			phraseToScroll.copy(c, 16);
+			c[16] = '\0';
+			(*lcd) << (unsigned char*)c;
+		}
+		else {
+			scrollPhase = false;
+			beginningOfPhrase = false;
+			timeBetweenScrolls = interval(0);
+			lastScroll = system_clock::now();
+			actualPosition = 0;
+
+			//Impresion unica del nombre
+			(*lcd) << (unsigned char*)phraseToScroll.c_str();
+			cursorPosition secLine;
+			secLine.row = 1;
+			secLine.column = 0;
+			lcd->lcdSetCursorPosition(secLine);
+
+		}
+		(*lcd) << (unsigned char*) "LOADING";
+		cursorPosition lastChar;
+		lastChar.row = 1;
+		lastChar.column = 15;
+		lcd->lcdSetCursorPosition(lastChar);
+		(*lcd) << '|';
+		currentLoadSymbol = 0;
+		loadSymbolTime = system_clock::now();
+		break;
+
+	}
+}
+
+void Twitter_LCD_Hitachi::control_NoChanges(void)
+{
+	switch (status) {
+	case statusType::LOADING:
+		//actualiza simbolo de carga
+		if (system_clock::now() >= (loadSymbolTime + interval(LOADING_INTERVAL))) {
+			loadSymbolTime = system_clock::now();
+			currentLoadSymbol++;
+			currentLoadSymbol %= sizeof(LoadingArray) - 1;
+
+			cursorPosition lastChar;
+			lastChar.row = 1;
+			lastChar.column = 15;
+			lcd->lcdSetCursorPosition(lastChar);
+			(*lcd) << (unsigned char)LoadingArray[currentLoadSymbol];
+		}
+
+		if (scrollPhase == true && system_clock::now() >= lastScroll + timeBetweenScrolls) {
+			lastScroll = system_clock::now();
+			actualPosition++;
+			if (actualPosition <= sizeofPhrase) {
+
+			}
+			else {
+				actualPosition = 0;
+
+			}
+		}
+
+
+
+		break;
+	default:
+		//Do nothing; el Display se queda mostrando el mensaje actual
+		break;
 	}
 }
--- pruebasConFTDI/Twitter_LCD_Hitachi.h
+++ pruebasConFTDI/Twitter_LCD_Hitachi.h
@@ -3,8 +3,11 @@
 #include <chrono>
 #include <ratio>
 
-using namespace std;
-using namespace std::chrono;
+
+
+typedef unsigned int time_interval_type;
+typedef std::chrono::duration<time_interval_type, std::ratio<1, 1000>> interval;
+
 
 class Twitter_LCD_Hitachi :
 	public Twitter_LCD
@@ -15,25 +18,31 @@ public:
 
 	virtual void step(void);
 private:
-	string phraseToScroll;
+	std::string phraseToScroll;
 	size_t sizeofPhrase;
 	bool scrollPhase;	//La frase es demasiado larga para el display? es decir, es necesario scrollearla?
 	bool beginningOfPhrase; //La frase a scrollear se encuentra en su posicion inicial. Es necesario que pase algo de tiempo antes de empezar a scrollearla (mayor al tiempo entre scrolls)
-	duration<int, ratio<1, 1000>> timeBetweenScrolls; //Tiempo entre scrolls
-	time_point<system_clock> lastScroll;
+	interval timeBetweenScrolls; //Tiempo entre scrolls
+	std::chrono::time_point<std::chrono::system_clock> lastScroll;
+	size_t actualPosition;
 
 	//Modo LOADING
 	unsigned char currentLoadSymbol; //contador 0 a 11, que hace el siguiente loop:   |{(<({|})>)}|
-	time_point<system_clock> loadSymbolTime;
+	std::chrono::time_point<std::chrono::system_clock> loadSymbolTime;
 
 
 
 
 
 	//MODO SHOW_TWEET
-	string formattedCurrent;
-	string formattedDate;
-	string formattedTweet;
+	std::string formattedCurrent;
+	std::string formattedDate;
+	std::chrono::time_point<std::chrono::system_clock> tweetStartTime;
 
+	void control_NewChanges(void);
+	void control_NoChanges(void);
+
+	void set_scrollingPhrase(void);
+	void manage_scrollingPhrase(unsigned int line);
 };
 
